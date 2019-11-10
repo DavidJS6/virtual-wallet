4f1a7f861bb39c41b60486a4f136fe8c100971ca
David Sandoval <davidjon2009@hotmail.com>
2019-11-09 21:19:50 -0400

    first commit

---
+++ src/test/java/soe/mdeis/modulo5/virtualwallet/VirtualWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package soe.mdeis.modulo5.virtualwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
