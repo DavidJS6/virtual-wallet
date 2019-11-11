85a32ee5da3cd1abc118e2f75b272dc84b02c2ab
DavidJS51 <davidjon2009@hotmail.com>
2019-11-11 14:50:38 -0400

    - Se hizo que la interfaz WalletService extienda de la interfaz ResourceService - Se elimino la implementacion anterior de la clase WalletServiceImpl y se implementaron los nueos métodos de la interfaz WalletService

--- src/main/java/soe/mdeis/modulo5/virtualwallet/web/wallets/service/WalletServiceImpl.java
+++ src/main/java/soe/mdeis/modulo5/virtualwallet/web/wallets/service/WalletServiceImpl.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import soe.mdeis.modulo5.virtualwallet.database.models.Wallet;
 import soe.mdeis.modulo5.virtualwallet.database.repositories.WalletRepository;
+import soe.mdeis.modulo5.virtualwallet.web.users.UserException;
 import soe.mdeis.modulo5.virtualwallet.web.users.service.UserService;
 import soe.mdeis.modulo5.virtualwallet.web.wallets.WalletRequestDto;
+import soe.mdeis.modulo5.virtualwallet.web.wallets.WalletResponseDto;
 
 import java.util.List;
 
@@ -22,38 +24,27 @@ public class WalletServiceImpl implements WalletService {
     }
 
     @Override
-    public Wallet save(WalletRequestDto request) {
-        Wallet wallet = new Wallet();
-        wallet.setWalletNumber(request.getWallet_number());
-        //wallet.setBalance(request.getBalance());
-        wallet.setUsersByUserId(userService.findById(request.getUser_id()));
-        return walletRepository.save(wallet);
+    public List<WalletResponseDto> findAll() {
+        return null;
     }
 
     @Override
-    public Wallet update(int id, WalletRequestDto request) {
-        Wallet wallet = findById(id);
-        wallet.setWalletNumber(request.getWallet_number());
-        //wallet.setBalance(request.getBalance());
-        wallet.setUsersByUserId(userService.findById(request.getUser_id()));
-        return walletRepository.save(wallet);
+    public WalletResponseDto findById(int id) throws UserException {
+        return null;
     }
 
     @Override
-    public boolean delete(int id) {
-        Wallet wallet = findById(id);
-        walletRepository.delete(wallet);
-        return true;
+    public WalletResponseDto update(int id, WalletRequestDto requestDto) {
+        return null;
     }
 
     @Override
-    public Wallet findById(int id) {
-        return walletRepository.findById(id).orElse(null);
+    public WalletResponseDto store(WalletRequestDto requestDto) {
+        return null;
     }
 
     @Override
-    public List<Wallet> findAll() {
-        return walletRepository.findAll();
-    }
+    public void delete(int id) {
 
+    }
 }
